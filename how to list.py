3dd599402282e02a5f1df9a21a573907bfaf2cd4
so mitake <player724@gmail.com>
2020-03-08 19:06:42 +0900

    [Continue] How to use list

--- how to list.py
+++ how to list.py
@@ -1,4 +1,6 @@
 # pythonと配列
+from typing import List
+
 l = [1, 2, 3, 5, 7, 11]
 print(l[3])
 print(l[0:2])
@@ -14,9 +16,109 @@ n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(n[::2])
 # ２つ飛ばしのやり方
 print(n[::-1])
-a = ['a', 'b' ,'c']
+a = ['a', 'b', 'c']
 b = [1, 2, 3]
 x = [a, b]
 print(x)
 print(x[0][2])
-print(x[1][1])
+print(x[1][0])
+# 配列を代入でき、その内容を指定して出力する方法
+
+s = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(s)
+s[0] = 'x'
+# s[0]にｘを代入
+print(s)
+s[2:5] = ['C', 'D', 'E']
+print(s)
+# s[2:5]の配列に代入
+s[2:5] = []
+print(s[:])
+# 空の[]を代入すると、配列から削除される
+
+w = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(w)
+w.append(25)
+w.insert(1, 21)
+print(w)
+# 関数append配列の最後に値を代入
+# insertは指定個所に値を代入
+w.pop(0)
+print(w)
+# 関数popは指定された場所の値を配列から取り除く。指定しないと最後の要素を削除
+del w[0]
+print(w)
+# 関数delは指定された場所の値を配列から取り除く。
+# del ステートメントは削除された値を返さない
+# 例えば、 del w を実行すると変数自体が削除される
+list = ['Mike', 'Alex', 'Francis']
+list.remove('Alex')
+print(list)
+# removeは配列内の特定の値を削除する（今までは配列の場所を指定していた）
+z = [1, 2, 3, 4, 5]
+y = [6, 7, 8, 9, 10]
+# k = z + y
+# print(k)
+z += y
+print(z)
+# x.extend(y)
+r = [1, 2, 3, 4, 5, 3, 6, 7, 8, 9, 10]
+print(r.index(3))
+# .indexは指定した値のある場所を選択する
+print(r.count(3))
+#  .indexは指定した値の個数を選択する
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+j = 'My name is Mike.'
+to_split = j.split(' ')
+print(to_split)
+# 順番を変える関数
+
+h = [1, 2, 3, 4, 5]
+c = h
+h[0] = 100
+print('h =', h)
+print('h =', c)
+g = [1, 2, 3, 4, 5]
+q = g.copy()
+q[0] = 100
+print(g)
+print(q)
+
+t = (1, 2, 3, 3, 4, 5)
+# tupleは基本的に代入などができない　Append スライスといった関数は使える。
+# 読み込み専用の配列として使うことが多い
+t = ([1, 2, 3], [4, 5, 6])
+print(t[0][0])
+(t[0][0]) = 100
+print(t[0][0])
+# ただし上記のようなことはできる。
+# t = 1, カンマがあるとtuple型になる　バグの原因になりやすい
+# int + tpl はできない
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+x, y = 10, 20
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+# tuple要素が長くなりすぎる場合、
+
+o = 10
+p = 20
+tmp = o
+o = p
+p = tmp
+print(o, p)
+# これが普通の入れ替え方
+
+rev1 = 100
+rev2 = 200
+rev1, rev2 = rev2, rev1
+print(rev1, rev2)
