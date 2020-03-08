f2c7e1c71722a2d354ee8e1976a48a607e3f3a24
so mitake <player724@gmail.com>
2020-03-08 16:20:10 +0900

    [START] How to use list

---
+++ how to list.py
@@ -0,0 +1,22 @@
+# pythonと配列
+l = [1, 2, 3, 5, 7, 11]
+print(l[3])
+print(l[0:2])
+print(l[2:])
+print(l[:2])
+print(l[-2])
+print(l[-2:])
+print(l[:-2])
+print(l[2:7])
+print(len(l))
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(n[::2])
+# ２つ飛ばしのやり方
+print(n[::-1])
+a = ['a', 'b' ,'c']
+b = [1, 2, 3]
+x = [a, b]
+print(x)
+print(x[0][2])
+print(x[1][1])
